4cc685ff79a45d6de8a39975d7c6e119e6f3d5b5
= <=>
2026-02-09 19:42:11 +0500

    Loading.tsx creation as suspense

--- src/app/(general)/blog/page.tsx
+++ src/app/(general)/blog/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
-export default function BlogPage() {
+export default async function BlogPage() {
+  await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a delay for loading
   return (
     <main>
       <h1>Welcome to the Blog Page</h1>
