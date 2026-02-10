1d800284d19617fd235ed19f773166f8a2b53222
= <=>
2026-02-10 18:20:37 +0500

    handling errors in layouts

--- src/app/(general)/products/[productId]/layout.tsx
+++ src/app/(general)/products/[productId]/layout.tsx
@@ -1,4 +1,7 @@
 export default function ProductDetailLayout({ children }: { children: React.ReactNode }) {
+  if (Math.floor(Math.random() * 2)) {
+    throw new Error("Random error occurred while fetching product details");
+  }
   return (
     <html>
       <body>
