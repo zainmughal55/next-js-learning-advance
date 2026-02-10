a4230a4b4c54642860a8b11bd54b771c12f453fb
= <=>
2026-02-10 18:03:55 +0500

    Recovering from Errors

--- src/app/(general)/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/(general)/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,5 +1,19 @@
 "use client";
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
 
-export default function ErrorBoundary({ error }: { error: Error }) {
-  return <h1 className="text-red-400">Something went wrong while loading the review details: {error.message}</h1>;
+export default function ErrorBoundary({ error, reset }: { error: Error, reset: () => void }) {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    })
+  }
+  return (
+    <div>
+      <h1 className="text-red-400">Something went wrong while loading the review details: {error.message}</h1>
+      <button onClick={reload}>Try Again</button>
+    </div>
+  )
 }
