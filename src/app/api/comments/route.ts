89c788d9d98a4d3112e0f6a4556edc6e4960de86
= <=>
2026-02-18 16:51:51 +0500

    handling POST Requests

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,5 +1,18 @@
-import {comments} from './data';
+import { comments } from './data';
 
 export async function GET() {
   return Response.json(comments);
+}
+
+export async function POST(request: Request) {
+  const { text } = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    text,
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: { 'Content-Type': 'application/json' },
+    status: 201
+  });
 }
