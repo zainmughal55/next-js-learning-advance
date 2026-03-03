01e06622f299fac5f3a177cbd20ea1bf2a199844
= <=>
2026-03-03 15:31:24 +0500

    Headers in Route Handlers

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,3 +1,10 @@
-export async function GET() {
-  return new Response("Profile API Route");
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  // const requestHeaders = new Headers(request.headers);
+  // console.log(requestHeaders.get("Authorization"));
+  const headerList = await headers();
+  console.log(headerList.get("Authorization"));
+  return new Response("<h1>Profile API Route</h1>", { headers: { "Content-Type": "text/html" } });
 }
