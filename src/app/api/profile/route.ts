4549269f23338e090699f9ae1099758ad378ed6f
= <=>
2026-03-03 15:40:45 +0500

    Cookies in Route Handlers

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -6,5 +6,19 @@ export async function GET(request: NextRequest) {
   // console.log(requestHeaders.get("Authorization"));
   const headerList = await headers();
   console.log(headerList.get("Authorization"));
-  return new Response("<h1>Profile API Route</h1>", { headers: { "Content-Type": "text/html" } });
+
+  const theme = request.cookies.get("theme");
+  console.log("Theme from cookies:", theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("limit", '20');
+  const limit = cookieStore.get("limit");
+  console.log("Limit from cookie store:", limit);
+
+  return new Response("<h1>Profile API Route</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    }
+  });
 }
